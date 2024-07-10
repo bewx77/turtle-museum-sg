a8baf757f5262ea7956391e96e944515329274e2
bewx77 <112966395+bewx77@users.noreply.github.com>
2024-07-10 13:02:57 +0800

    Fit device size to builder.io dimensions

--- frontend/my-react-app/tailwind.config.js
+++ frontend/my-react-app/tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': {'max': '640px'},      // Small devices (Mobile)
+      'md': {'min': '641px', 'max': '991px'},  // Tablets
+      'lg': {'min': '992px'},      // Desktops
+    },
     extend: {},
   },
   plugins: [],
